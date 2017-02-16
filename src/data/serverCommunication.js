5ffa1b4ee2b5389e0db482a91dcd0f2e25e3c4b9
Dor <dor@infinigrow.com>
2017-02-16 15:21:11 +0200

    added production configuration

--- src/data/serverCommunication.js
+++ src/data/serverCommunication.js
@@ -15,7 +15,8 @@ export default {
 		var deferred = q.defer();
 
 		//fetch('http://localhost:3000/' + route, {
-		fetch('http://infinigrowtest.centralus.cloudapp.azure.com:3000/' + route, {
+		//fetch('http://infinigrowtest.centralus.cloudapp.azure.com:3000/' + route, {
+		fetch('http://13.82.230.159:3000/' + route, {
 		method: httpFunc,
 			headers: {
 				'Content-Type': 'application/json'
