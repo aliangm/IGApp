4999c580c999d58262de982f63073d561ca725dc
Dor <dor@infinigrow.com>
2017-08-10 12:59:55 +0300

    bug fix in active campaigns

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -34,7 +34,7 @@ export default class Dashboard extends Component {
       opps: 0,
       users: 0
     },
-    campaigns: {},
+    unfilteredCampaigns: {},
     objectives: [],
     annualBudgetArray: [],
     previousData: []
@@ -73,7 +73,7 @@ export default class Dashboard extends Component {
   }
 
   render() {
-    const { planDate, projectedPlan, actualIndicators, campaigns, objectives, annualBudgetArray, previousData } = this.props;
+    const { planDate, projectedPlan, actualIndicators, unfilteredCampaigns, objectives, annualBudgetArray, previousData } = this.props;
     const planJson = parseAnnualPlan(projectedPlan);
     const planData = planJson[Object.keys(planJson)[0]];
     const planDataChannels = Object.keys(planData).filter(channelName => channelName !== '__TOTAL__');
@@ -82,9 +82,9 @@ export default class Dashboard extends Component {
       .filter(channelName => channelName !== '__TOTAL__' && planData[channelName].values[0] !== 0)
       .map((fatherChannel)=> { return { name: fatherChannel, value: planData[fatherChannel].values[0] } });
     const budgetLeftToPlan = annualBudgetArray.reduce((a, b) => a + b, 0) - planData['__TOTAL__'].values.reduce((a, b) => a + b, 0);
-    const numberOfActiveCampaigns = Object.keys(campaigns).map((channel) =>
+    const numberOfActiveCampaigns = Object.keys(unfilteredCampaigns).map((channel) =>
     {
-      return campaigns[channel].filter(campaign=>  campaign.status !== 'Completed' && campaign.isArchived !== true ).length;
+      return unfilteredCampaigns[channel].filter(campaign=>  campaign.status !== 'Completed' && campaign.isArchived !== true ).length;
     }).reduce((a, b) => a + b, 0);
     const ratio = (actualIndicators.LTV/actualIndicators.CAC).toFixed(2) || 0;
     const COLORS = [
