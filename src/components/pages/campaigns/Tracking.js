c7a48e358d2437983d20322c29d0f1a695a64a1c
Dor <dor@infinigrow.com>
2017-10-31 09:50:32 +0200

    source medium and campaign UTMs are mandatory

--- src/components/pages/campaigns/Tracking.js
+++ src/components/pages/campaigns/Tracking.js
@@ -91,39 +91,50 @@ export default class Tracking extends Component {
     update.tracking.utms = this.state.utms;
     update.tracking.campaignUTM = this.state.campaignUTM;
     update.source.forEach((source, index) => {
-      const url = (update.tracking.isHttp ? 'http://' : 'https://') +
-        update.tracking.baseUrl +
-        '/?utm_source=' + this.state.utms[index].source +
-        '&utm_medium=' + this.state.utms[index].medium +
-        '&utm_campaign=' + this.state.campaignUTM +
-        (this.state.utms[index].content ? '&utm_content=' + this.state.utms[index].content : '' )+
-        (this.state.utms[index].term ? '&utm_term=' + this.state.utms[index].term : '');
-      fetch('https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyDEoi0JNfWmDlnN8swZz_tZc3Vu14yV0rw', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({longUrl: encodeURI(url)})
-      })
-        .then((response) => {
-          response.json()
-            .then((data) => {
-              if (data) {
-                update.tracking.urls.splice(index, 0, {
-                  long: data.longUrl,
-                  short: data.id,
-                  createDate: new Date()
-                });
-                if (update.tracking.urls.length === update.source.length) {
-                  this.props.updateState({campaign: update, unsaved: false});
-                  this.props.updateCampaign(update);
-                }
-              }
-            })
+      if (!this.state.campaignUTM) {
+        this.refs.campaign.focus();
+      }
+      else if (!this.state.utms[index].source || this.state.utms[index].source.includes("(")) {
+        this.refs["source" + index].focus();
+      }
+      else if (!this.state.utms[index].medium) {
+        this.refs["medium" + index].focus();
+      }
+      else {
+        const url = (update.tracking.isHttp ? 'http://' : 'https://') +
+          update.tracking.baseUrl +
+          '/?utm_source=' + this.state.utms[index].source +
+          '&utm_medium=' + this.state.utms[index].medium +
+          '&utm_campaign=' + this.state.campaignUTM +
+          (this.state.utms[index].content ? '&utm_content=' + this.state.utms[index].content : '' ) +
+          (this.state.utms[index].term ? '&utm_term=' + this.state.utms[index].term : '');
+        fetch('https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyDEoi0JNfWmDlnN8swZz_tZc3Vu14yV0rw', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({longUrl: encodeURI(url)})
         })
-        .catch((error) => {
-          console.log(error);
-        });
+          .then((response) => {
+            response.json()
+              .then((data) => {
+                if (data) {
+                  update.tracking.urls.splice(index, 0, {
+                    long: data.longUrl,
+                    short: data.id,
+                    createDate: new Date()
+                  });
+                  if (update.tracking.urls.length === update.source.length) {
+                    this.props.updateState({campaign: update, unsaved: false});
+                    this.props.updateCampaign(update);
+                  }
+                }
+              })
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
     })
   }
 
@@ -153,11 +164,12 @@ export default class Tracking extends Component {
             value={ isBrackets ? '' : this.state.utms[index].source }
             onChange={ this.handleChangeUTM.bind(this, 'source', index) }
             placeHolder={ isBrackets ? this.state.utms[index].source.replace(/[()]/g, "") : '' }
+            ref={"source" + index}
           />
         </div>
         <div className={ this.classes.row }>
           <Label>{"Medium " + (index+1)}</Label>
-          <Textfield value={ this.state.utms[index].medium } onChange={ this.handleChangeUTM.bind(this, 'medium', index) }/>
+          <Textfield value={ this.state.utms[index].medium } onChange={ this.handleChangeUTM.bind(this, 'medium', index) } ref={"medium" + index}/>
         </div>
         <div className={ this.classes.row }>
           <div className={ trackingStyle.locals.advanced } onClick={ this.toggleAdvanced.bind(this, index) }>Advanced</div>
@@ -214,7 +226,7 @@ export default class Tracking extends Component {
       </div>
       <div className={ this.classes.row }>
         <Label>Campaign</Label>
-        <Textfield value={ this.state.campaignUTM } onChange={ (e)=>{ this.setState({campaignUTM: e.target.value}) } }/>
+        <Textfield value={ this.state.campaignUTM } onChange={ (e)=>{ this.setState({campaignUTM: e.target.value}) } } ref={"campaign"}/>
       </div>
       { sources }
       <div className={ trackingStyle.locals.rowCenter }>
