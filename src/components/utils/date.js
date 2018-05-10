9a36261ebb2730a8e3a83265fa7baec0f6d11f68
Dor <dor@infinigrow.com>
2018-05-10 11:18:03 +0300

    bug fix in dates util

--- src/components/utils/date.js
+++ src/components/utils/date.js
@@ -12,9 +12,9 @@ export function getDates(dateStr) {
   if (dateStr) {
     const dates = [];
     const planDate = dateStr.split("/");
-    const date = new Date(planDate[1], planDate[0] - 1);
     const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     for (let i = 0; i < 12; i++) {
+      const date = new Date(planDate[1], planDate[0] - 1);
       date.setMonth(date.getMonth() + i);
       dates.push(monthNames[date.getMonth()] + '/' + date.getFullYear().toString().substr(2, 2));
     }
