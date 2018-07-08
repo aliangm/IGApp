5e5494ceb139706b42d367ff69e97d93c9e0a7fc
Or Cherfas <or@infinigrow.com>
2018-07-08 22:05:25 +0300

    removed unnecessary flex in style

--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -19,9 +19,9 @@ export default class Settings extends Component {
     return <div>
       <Page contentClassName={this.classes.content} innerClassName={this.classes.pageInner} width="100%">
         <SettingsSideBar currentPath={this.props.location.pathname}/>
-          <div style={{flex:'1'}}>
-            {childrenWithProps}
-          </div>
+        <div>
+          {childrenWithProps}
+        </div>
       </Page>
     </div>
   }
