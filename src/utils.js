1a30820e9af06d468914fd6f588d9452c8c98425
Or Cherfas <or@infinigrow.com>
2018-07-09 17:15:15 +0300

    added space

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 
-export function getExtarpolateRatio(day, monthString){
+export function getExtarpolateRatio(day, monthString) {
   const planDate = monthString.split("/");
   const date = new Date(planDate[1], planDate[0] - 1);
   const numberOfDaysInMonth = 32 - new Date(date.getFullYear(), date.getMonth(), 32).getDate();
