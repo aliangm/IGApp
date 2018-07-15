e9a5e71e817a611400c40563f5c433da2b7edacc
Or Cherfas <or@infinigrow.com>
2018-07-15 17:19:13 +0300

    tabs of dashboard now on top bar

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -10,28 +10,13 @@ export default class Dashboard extends Component {
   style = style;
 
   render() {
-    const tabs = {
-      CMO: '/dashboard/CMO',
-      Metrics: '/dashboard/metrics'
-    };
 
-    const tabNames = Object.keys(tabs);
     const childrenWithProps = React.Children.map(this.props.children,
       (child) => React.cloneElement(child, this.props));
     return <div>
       <Page contentClassName={ this.classes.content } innerClassName={ this.classes.pageInner } width="100%">
         <div className={ this.classes.head }>
           <div className={ this.classes.headTitle }>Dashboard</div>
-          <div className={ this.classes.headTabs }>
-            {
-              tabNames.map((name, i) => {
-                const link = Object.values(tabs)[i];
-                return <Link to={ link } activeClassName={this.classes.headTabSelected} className={this.classes.headTab} key={ i }>
-                  { name }
-                </Link>
-              })
-            }
-          </div>
         </div>
         { this.props.userAccount.pages && this.props.userAccount.pages.dashboard ?
           <div>
