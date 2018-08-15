0a2a6cd60e39bac1660dc7a9c24227a05e929f38
Or Cherfas <or@infinigrow.com>
2018-08-15 15:02:49 +0300

    custom options step css

--- src/components/pages/plan/ConstraintStep.js
+++ src/components/pages/plan/ConstraintStep.js
@@ -37,43 +37,50 @@ export default class ConstraintStep extends Component {
     const disableInput = this.state.setClicked;
     const beforeInputText = this.isChannelsLimitConstraint() ? `Don't touch more than` : `Don’t touch`;
 
-    return <div className={this.classes.constraintStep}>
-      <span className={this.classes.constraintText}>{beforeInputText}</span>
-      {this.props.type === 'channelsNumber'
-        ? <Textfield type="number" value={this.state.changeObject.channelsLimit}
-                     onChange={(e) =>
-                       this.setState({
-                         changeObject: {
-                           channelsLimit: parseInt(e.target.value)
-                         }
-                       })
-                     } min="2"
-                     disabled={disableInput}/>
-        : <MultiSelect disabled={disableInput}
-                       selected={this.state.changeObject.channelsToBlock}
-                       select={{
-                         name: 'Channels',
-                         options: [
-                           {value: 'first', label: 'first label'},
-                           {value: 'second', label: 'second label'},
-                           {value: 'third', label: 'third label'}]
-                       }}
+    return <div className={this.classes.optionsWrapper}>
+        <div className={this.classes.constraintStep}>
+        <span className={this.classes.constraintText}>{beforeInputText}</span>
+        {this.props.type === 'channelsNumber'
+          ? <Textfield type="number" value={this.state.changeObject.channelsLimit}
+                       onChange={(e) =>
+                         this.setState({
+                           changeObject: {
+                             channelsLimit: parseInt(e.target.value)
+                           }
+                         })
+                       } min="2"
+                       disabled={disableInput}
+                       className={this.classes.inputField}
+                       inputClassName={this.classes.numberInput}/>
+          : <MultiSelect disabled={disableInput}
+                         selected={this.state.changeObject.channelsToBlock}
+                         select={{
+                           name: 'Channels',
+                           options: [
+                             {value: 'first', label: 'first label'},
+                             {value: 'second', label: 'second label'},
+                             {value: 'third', label: 'third label'}]
+                         }}
 
-                       onChange={this.handleChangeMulti}/>
+                         onChange={this.handleChangeMulti}
+                         className={this.classes.inputField}/>
 
-      }
-      {this.isChannelsLimitConstraint ? <span className={this.classes.constraintText}>channels</span> : null}
-      <div className={this.classes.constraintText} onClick={() => {
-        if (!this.state.setClicked) {
-          this.setState({setClicked: true});
-          this.props.setConstraintAndRunPlanner(this.state.changeObject,
-            () => this.props.triggerNextStep({
-              trigger: '7'
-            })
-          );
         }
-      }}>
-        Set
+        {this.isChannelsLimitConstraint ? <span className={this.classes.constraintText}>channels</span> : null}
+        <div className={this.classes.option}
+             data-chosen={this.state.setClicked ? true : null}
+             onClick={() => {
+                if (!this.state.setClicked) {
+                  this.setState({setClicked: true});
+                  this.props.setConstraintAndRunPlanner(this.state.changeObject,
+                    () => this.props.triggerNextStep({
+                      trigger: '7'
+                    })
+                  );
+                }
+        }}>
+          Set
+        </div>
       </div>
     </div>;
   }
