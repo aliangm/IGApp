5669bd756d2540ca5d7f059685c59c9e6bbb483d
Dor <dor@infinigrow.com>
2018-08-16 12:15:22 +0300

    insight item design improvement and props support

--- src/components/pages/plan/InsightItem.js
+++ src/components/pages/plan/InsightItem.js
@@ -1,20 +1,109 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import Component from 'components/Component';
 import style from 'styles/plan/insight-item.css';
 import Button from 'components/controls/Button';
 import ReactTooltip from 'react-tooltip';
+import {formatBudget} from 'components/utils/budget';
+import groupBy from 'lodash/groupBy';
+import {getNickname as getIndicatorNickname} from 'components/utils/indicators';
 
 export default class InsightItem extends Component {
 
   style = style;
 
-  getTooltip = () =>
-    `<div style="text-align: center">
-      MAR 18<br/>
-      MQLs <span class="arrow">144%</span> (+33)<br/> 
+  static propTypes = {
+    fromChannels: PropTypes.arrayOf(PropTypes.shape({
+      fromBudget: PropTypes.number.isRequired,
+      toBudget: PropTypes.number.isRequired,
+      channel: PropTypes.any.isRequired,
+      month: PropTypes.any.isRequired
+    })).isRequired,
+    toChannels: PropTypes.arrayOf(PropTypes.shape({
+      fromBudget: PropTypes.number.isRequired,
+      toBudget: PropTypes.number.isRequired,
+      channel: PropTypes.any.isRequired,
+      month: PropTypes.any.isRequired
+    })).isRequired,
+    forecasting: PropTypes.arrayOf(PropTypes.shape({
+      committed: PropTypes.number.isRequired,
+      ifApproved: PropTypes.number.isRequired,
+      indicator: PropTypes.any.isRequired,
+      month: PropTypes.any.isRequired
+    })).isRequired,
+    onCommit: PropTypes.func.isRequired,
+    onDecline: PropTypes.func.isRequired
+  };
+
+  static defaultProps = {
+    fromChannels: [{
+      fromBudget: 1000,
+      toBudget: 2000,
+      channel: 'web_landingPages',
+      month: 'APR 18'
+    },
+      {
+        fromBudget: 1000,
+        toBudget: 2000,
+        channel: 'web_landingPages',
+        month: 'APR 18'
+      }
+    ],
+    toChannels: [{
+      fromBudget: 1000,
+      toBudget: 2000,
+      channel: 'web_landingPages',
+      month: 'APR 18'
+    },
+      {
+        fromBudget: 1000,
+        toBudget: 2000,
+        channel: 'web_landingPages',
+        month: 'APR 18'
+      }
+    ],
+    forecasting: [{
+      committed: 40,
+      ifApproved: 50,
+      indicator: 'MQL',
+      month: 'APR 18'
+    },
+      {
+        committed: 40,
+        ifApproved: 50,
+        indicator: 'opps',
+        month: 'APR 18'
+      },
+      {
+        committed: 40,
+        ifApproved: 50,
+        indicator: 'opps',
+        month: 'MAY 18'
+      }
+    ]
+  };
+
+  getTooltip = () => {
+    const forecastingData = groupBy(this.props.forecasting, (item) => item.month);
+    return Object.keys(forecastingData).map(month => {
+      const monthIndicators = forecastingData[month]
+        .map(item =>
+          `<div style="text-align: left; font-size: 10px; font-weight: 500">
+       ${getIndicatorNickname(item.indicator)} <span style="color: #24b10e">${Math.round(item.ifApproved / item.committed * 100)}%</span> (+${item.ifApproved - item.committed})
+        </div><br/>`
+        );
+      return `<div style="text-align: center; font-size: 10px; font-weight: 500">
+        ${month}
+        </div><br/>
+      <div>
+      ${monthIndicators.join('')}
     </div>`;
+    }).join('');
+  };
 
   render() {
+    const {fromChannels, toChannels} = this.props;
+    const fromChannelsItems = fromChannels.map((item, index) => <ChannelItem key={index} {...item}/>);
+    const toChannelsItems = toChannels.map((item, index) => <ChannelItem key={index} {...item}/>);
 
     return <div>
       <ReactTooltip place='bottom' effect='solid' id='insightItem' html={true}/>
@@ -24,10 +113,13 @@ export default class InsightItem extends Component {
           <div className={this.classes.forecastingIcon} data-tip={this.getTooltip()} data-for='insightItem'/>
         </div>
         <div className={this.classes.inner}>
-          <ChannelItem/>
+          <div>
+            {fromChannelsItems}
+          </div>
           <div className={this.classes.arrowIcon}/>
-          <ChannelItem/>
-          <ChannelItem/>
+          <div>
+            {toChannelsItems}
+          </div>
         </div>
       </div>
       <div className={this.classes.buttons}>
@@ -47,19 +139,21 @@ export class ChannelItem extends Component {
   style = style;
 
   render() {
-
-    return <div className={this.classes.channelItem}>
-      <div className={this.classes.date}>
-        MAR 18
-      </div>
-      <div className={this.classes.channelIcon} data-icon="plan:TV"/>
-      <div style={{display: 'flex'}}>
-        <div className={this.classes.fromBudget}>
-          $7,000
+    const {fromBudget, toBudget, channel, month} = this.props;
+    return <div style={{display: 'inline-flex'}}>
+      <div className={this.classes.channelItem}>
+        <div className={this.classes.date}>
+          {month}
         </div>
-        <div className={this.classes.shiftIcon}/>
-        <div className={this.classes.toBudget}>
-          $9,000
+        <div className={this.classes.channelIcon} data-icon={`plan:${channel}`}/>
+        <div style={{display: 'flex'}}>
+          <div className={this.classes.fromBudget}>
+            {formatBudget(fromBudget)}
+          </div>
+          <div className={this.classes.shiftIcon}/>
+          <div className={this.classes.toBudget}>
+            {formatBudget(toBudget)}
+          </div>
         </div>
       </div>
     </div>;
