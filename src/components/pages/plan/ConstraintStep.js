ec38a5456130d63002ebff83be705713f06d331a
Dor <dor@infinigrow.com>
2018-08-16 18:38:25 +0300

    constraint step design fix

--- src/components/pages/plan/ConstraintStep.js
+++ src/components/pages/plan/ConstraintStep.js
@@ -43,11 +43,11 @@ export default class ConstraintStep extends Component {
       return {
         value: channelKey,
         label: getNickname(channelKey)
-      }
+      };
     });
 
     return <div className={this.classes.optionsWrapper}>
-        <div className={this.classes.constraintStep}>
+      <div className={this.classes.constraintStep}>
         <span className={this.classes.constraintText}>{beforeInputText}</span>
         {this.props.type === 'channelsNumber'
           ? <Textfield type="number" value={this.state.changeObject.channelsLimit}
@@ -65,22 +65,24 @@ export default class ConstraintStep extends Component {
                          selected={this.state.changeObject.channelsToBlock}
                          select={{name: 'Channels', options: channelsOptions}}
                          onChange={this.handleChangeMulti}
-                         className={this.classes.inputField}/>
+                         className={this.classes.inputField}
+                         style={{width: '250px'}}
+          />
 
         }
-        {this.isChannelsLimitConstraint ? <span className={this.classes.constraintText}>channels</span> : null}
+        {this.isChannelsLimitConstraint() ? <span className={this.classes.constraintText}>channels</span> : null}
         <div className={this.classes.setButton}
              data-chosen={this.state.setClicked ? true : null}
              onClick={() => {
-                if (!this.state.setClicked) {
-                  this.setState({setClicked: true});
-                  this.props.setConstraintAndRunPlanner(this.state.changeObject,
-                    () => this.props.triggerNextStep({
-                      trigger: '7'
-                    })
-                  );
-                }
-        }}>
+               if (!this.state.setClicked) {
+                 this.setState({setClicked: true});
+                 this.props.setConstraintAndRunPlanner(this.state.changeObject,
+                   () => this.props.triggerNextStep({
+                     trigger: '7'
+                   })
+                 );
+               }
+             }}>
           Set
         </div>
       </div>
