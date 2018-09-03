660013863d4f4366a225413bbe1e83d75edf8f0e
Slobodan Radosavljevic <slobodanmeister@gmail.com>
2018-09-03 18:02:25 +0200

    Add cross browser style fixes

--- src/components/controls/FloatingComponent.js
+++ src/components/controls/FloatingComponent.js
@@ -4,14 +4,14 @@ import style from 'styles/controls/floating-component.css';
 
 
 const LeftTabCountour = () => (
-    <svg version="1.1" id="left-contour" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24px" height="29px" viewBox="0 0 24 29" enable-background="new 0 0 24 29" xmlSpace="preserve">
+    <svg version="1.1" id="left-contour" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24px" height="29px" viewBox="0 0 24 29" enableBackground="new 0 0 24 29" xmlSpace="preserve">
         <path fill="#FFFFFF" d="M24,0v29H0c4.34-1.34,7.17-2.67,8.5-4s2.991-3.896,3.5-8c0.154-1.24,0.026-3.75,0-5 C11.864,5.371,17.37,0,24,0z"/>
     </svg>
 )
 
 const RightTabCountour = () => (
     <svg version="1.1" id="right-countour" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-        width="24px" height="29px" viewBox="0 0 24 29" enable-background="new 0 0 24 29" xmlSpace="preserve">
+        width="24px" height="29px" viewBox="0 0 24 29" enableBackground="new 0 0 24 29" xmlSpace="preserve">
         <path fill="#FFFFFF" d="M24,29H0V0c6.63,0,12,5.37,12,12c0,0-0.154,3.76,0,5c0.509,4.104,2.17,6.67,3.5,8S19.66,27.66,24,29z"/>
     </svg>
 );
@@ -177,7 +177,7 @@ export default class FloatingComponent extends Component {
                         <div className={this.classes.controlHandle} onClick={this.toggleActive}>
                             <span className={this.classes.controlIconWrapper}>
                                 <svg className={this.classes.controlIcon} xmlns="http://www.w3.org/2000/svg" width="7" height="4" viewBox="0 0 7 4">
-                                    <path fill="#B2BBD5" fill-rule="nonzero" d="M.703 0L0 .719l3.477 3.34L6.949.719 6.254 0 3.477 2.66z"/>
+                                    <path fill="#B2BBD5" fillRule="nonzero" d="M.703 0L0 .719l3.477 3.34L6.949.719 6.254 0 3.477 2.66z"/>
                                 </svg>
                             </span>
                             <span className={this.classes.controlText}>{controlText}</span>
