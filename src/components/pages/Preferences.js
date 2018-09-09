7801d811bdbb9cc2bcc419ab1485415bc969001b
Or Cherfas <or@infinigrow.com>
2018-09-09 09:43:35 +0300

    hide PlanFromExcel when not in onBoarding

--- src/components/pages/Preferences.js
+++ src/components/pages/Preferences.js
@@ -50,9 +50,13 @@ export default class Preferences extends Component {
     super(props);
     this.state = {
       isCheckAnnual: props.annualBudget !== null,
-      isDivideEqually: props.annualBudget !== null && props.annualBudgetArray.length > 0 && props.annualBudgetArray.every((budget) => {
-        return budget === props.annualBudgetArray[0];
-      }),
+      isDivideEqually: props.annualBudget !==
+        null &&
+        props.annualBudgetArray.length >
+        0 &&
+        props.annualBudgetArray.every((budget) => {
+          return budget === props.annualBudgetArray[0];
+        }),
       showAdvancedFields: false,
       objectivePopupData: {
         hidden: true,
@@ -213,7 +217,9 @@ export default class Preferences extends Component {
           for (let i = 0; i < 4; i++) {
             const index = (monthIndex + (i * 3)) % 12;
             let targetValue = -1;
-            const value = i ? recurrentArray[(monthIndex + ((i - 1) * 3)) % 12] : this.props.actualIndicators[objective];
+            const value = i
+              ? recurrentArray[(monthIndex + ((i - 1) * 3)) % 12]
+              : this.props.actualIndicators[objective];
             if (objectiveData.isPercentage) {
               targetValue = (objectiveData.amount / 100 + 1) * value;
             }
@@ -251,15 +257,20 @@ export default class Preferences extends Component {
       }
 
       // not the expected priority, need to replace
-      const expectedPriority = isEdit ? objectives[monthIndex][objective].target.priority : this.props.calculatedData.objectives.objectivesData.length;
+      const expectedPriority = isEdit
+        ? objectives[monthIndex][objective].target.priority
+        : this.props.calculatedData.objectives.objectivesData.length;
       if (objectiveData.priority !== expectedPriority) {
-        const previous = this.props.calculatedData.objectives.objectivesData.find(item => item.priority === objectiveData.priority);
+        const previous = this.props.calculatedData.objectives.objectivesData.find(
+          item => item.priority === objectiveData.priority);
         if (previous) {
           const {monthIndex, indicator} = previous;
           objectives[monthIndex][indicator].target.priority = expectedPriority;
         }
       }
-      const targetValue = objectiveData.isRecurrent ? recurrentArray.find(item => item !== -1) : objectiveData.targetValue;
+      const targetValue = objectiveData.isRecurrent
+        ? recurrentArray.find(item => item !== -1)
+        : objectiveData.targetValue;
       if (!isNil(monthIndex) && targetValue) {
         objectives[monthIndex][objective] = {
           target: {
@@ -296,7 +307,9 @@ export default class Preferences extends Component {
       return <div className={this.classes.cell} key={index}>
         <Label style={{width: '70px', marginTop: '12px'}}>{month}</Label>
         <Textfield
-          value={'$' + (this.props.annualBudgetArray[index] ? this.props.annualBudgetArray[index].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') : '')}
+          value={'$' +
+          (this.props.annualBudgetArray[index] ? this.props.annualBudgetArray[index].toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ',') : '')}
           onChange={this.handleChangeBudgetArray.bind(this, index)} style={{
           width: '166px'
         }}/>
@@ -405,7 +418,11 @@ export default class Preferences extends Component {
     const indicatorsWithProps = getIndicatorsWithProps();
     const objectiveOptions = Object.keys(indicatorsWithProps)
       .filter(indicatorKey => indicatorsWithProps[indicatorKey].isObjective &&
-        !objectivesData.find(item => item.indicator === indicatorKey && (this.state.objectivePopupData.objective ? item.indicator !== this.state.objectivePopupData.objective : true)))
+        !objectivesData.find(item => item.indicator ===
+          indicatorKey &&
+          (this.state.objectivePopupData.objective
+            ? item.indicator !== this.state.objectivePopupData.objective
+            : true)))
       .map(indicatorKey => {
         return {value: indicatorKey, label: indicatorsWithProps[indicatorKey].nickname};
       });
@@ -414,8 +431,10 @@ export default class Preferences extends Component {
 
     return <div>
       <Page popup={isPopupMode()} className={!isPopupMode() ? this.classes.static : null}>
-        {isPopupMode() ? <Title title="Preferences"
-                                subTitle="What are your marketing goals and constrains? Different objectives dictate different strategies"/> : null}
+        {isPopupMode()
+          ? <Title title="Preferences"
+                   subTitle="What are your marketing goals and constrains? Different objectives dictate different strategies"/>
+          : null}
         <div className={this.classes.error}>
           <label hidden={!this.props.serverDown}>Something is wrong... Let us check what is it and fix it for you
             :)</label>
@@ -435,7 +454,9 @@ export default class Preferences extends Component {
                 ($)</Label>
               <div className={this.classes.cell}>
                 <Textfield disabled={!this.state.isCheckAnnual}
-                           value={'$' + (this.props.annualBudget ? this.props.annualBudget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') : '')}
+                           value={'$' +
+                           (this.props.annualBudget ? this.props.annualBudget.toString()
+                             .replace(/\B(?=(\d{3})+(?!\d))/g, ',') : '')}
                            onChange={this.handleChangeBudget.bind(this, 'annualBudget')} style={{
                   width: '166px'
                 }}/>
@@ -558,10 +579,12 @@ export default class Preferences extends Component {
                             allowSameValues={true}
                             minValue={0}
                             maxValue={Math.min(...annualBudgetArray)}
-                            value={budgetConstraints[budgetConstraintsChannels[index]] ? budgetConstraints[budgetConstraintsChannels[index]].range : {
-                              min: 0,
-                              max: -1
-                            }}
+                            value={budgetConstraints[budgetConstraintsChannels[index]]
+                              ? budgetConstraints[budgetConstraintsChannels[index]].range
+                              : {
+                                min: 0,
+                                max: -1
+                              }}
                             onChange={this.handleRangeChange.bind(this, index)}
                           />
                           <div style={{marginLeft: '25px', alignSelf: 'center'}}>
@@ -586,9 +609,13 @@ export default class Preferences extends Component {
                 </div>
               </div>
             </FeatureToggle>
-            <div className={this.classes.row} style={{marginTop: '55px'}}>
-              <PlanFromExcel {...this.props}/>
-            </div>
+            {
+              isPopupMode() ?
+                <div className={this.classes.row} style={{marginTop: '55px'}}>
+                  <PlanFromExcel {...this.props}/>
+                </div>
+                : null
+            }
           </div>
 
           {isPopupMode() ?
