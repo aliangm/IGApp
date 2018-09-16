48ce1928134a4089d9931273c2b8bdae4de30b12
Or Cherfas <or@infinigrow.com>
2018-09-16 10:33:02 +0300

    added copy and tab selection to Attribution Setup. Added more styling

--- src/components/pages/attribution/Setup2.js
+++ src/components/pages/attribution/Setup2.js
@@ -1,40 +1,52 @@
 import React from 'react';
 import Component from 'components/Component';
-import Tabs from 'components/onboarding/Tabs';
 import style from 'styles/attribution/setup2.css';
 import copy from 'copy-to-clipboard';
-import Textfield from 'components/controls/Textfield';
-import Label from 'components/ControlsLabel';
-import Select from 'components/controls/Select';
 import Button from 'components/controls/Button';
 import Toggle from 'components/controls/Toggle';
+import Page from 'components/Page';
+import {isPopupMode} from 'modules/popup-mode';
+import onBoardingStyle from 'styles/onboarding/onboarding.css';
+import planStyles from 'styles/plan/plan.css';
 
 export default class Setup extends Component {
 
-  code = (userId) => `<script type="text/javascript" async=1>
-  ;(function (p, l, o, w, i, n, g) {
-    if (!p[i]) {
-      p.GlobalInfinigrowObject = {};
-      p.GlobalInfinigrowObject.userId = '${userId}';
-      p.GlobalInfinigrowObject.InfinigrowNamespace = i;
-
-      p[i] = function () {
-        (p[i].q = p[i].q || []).push(arguments);
-      };
-      p[i].q = p[i].q || [];
-
-      n = l.createElement(o);
-      g = l.getElementsByTagName(o)[0];
-      n.async = 1;
-      n.src = w;
-      g.parentNode.insertBefore(n, g);
-    }
-  }(window, document, 'script', "//ddzuuyx7zj81k.cloudfront.net/1.0.0/attributionSnippet.js", 'infinigrow'));
-</script>`;
-
   style = style;
+  styles = [onBoardingStyle, planStyles];
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tab: 0
+    };
+  }
 
   render() {
+    const {UID: userId} = this.props.userAccount;
+
+    const code =
+      `<script type="text/javascript" async=1>
+        ;(function (p, l, o, w, i, n, g) {
+          if (!p[i]) {
+            p.GlobalInfinigrowObject = {};
+            p.GlobalInfinigrowObject.userId = '${userId}';
+            p.GlobalInfinigrowObject.InfinigrowNamespace = i;
+      
+            p[i] = function () {
+              (p[i].q = p[i].q || []).push(arguments);
+            };
+            p[i].q = p[i].q || [];
+      
+            n = l.createElement(o);
+            g = l.getElementsByTagName(o)[0];
+            n.async = 1;
+            n.src = w;
+            g.parentNode.insertBefore(n, g);
+          }
+        }(window, document, 'script', "//ddzuuyx7zj81k.cloudfront.net/1.0.0/attributionSnippet.js", 'infinigrow'));
+      </script>`;
+
     const renderStep = (stepNumber, stepTitle, stepSubTitle, stepContent) => {
       return <div className={this.classes.step}>
         <div className={this.classes.stepHead}>
@@ -52,20 +64,31 @@ export default class Setup extends Component {
       </div>;
     };
 
-    return <div className={this.classes.page}>
+    return <Page popup={isPopupMode()}
+                 className={!isPopupMode() ? this.classes.static : null}
+                 contentClassName={onBoardingStyle.locals.content}
+                 innerClassName={onBoardingStyle.locals.pageInner}
+                 width='100%'>
+      <div className={this.classes.head}>
+        <div className={planStyles.locals.headTitle}>Attribution</div>
+      </div>
+      <div className={this.classes.title}>Add the tracking script to your website</div>
+      <div className={this.classes.subTitle}>Setting up InfiniGrow’s tracking is easy and takes about a minute. This is
+        the first and last time you'll be asked to use code.
+      </div>
       {
         renderStep(1, 'Copy your code', 'or get it sent by email with a step-by-step guide',
           <div className={this.classes.snippetBox}>
             <div className={this.classes.snippetWrapper}>
               <pre className={this.classes.snippet}>
-              {this.code('#userId#')}
+              {code}
               </pre>
             </div>
             <div className={this.classes.buttons}>
               <Button type='secondary' className={this.classes.leftButton} contClassName={this.classes.buttonContent}>Email
                 this script and instructions</Button>
               <Button type='primary' icon="buttons:edit" className={this.classes.rightButton}
-                      contClassName={this.classes.buttonContent}>Copy</Button>
+                      contClassName={this.classes.buttonContent} onClick={() => copy(code)}>Copy</Button>
             </div>
           </div>)
       }
@@ -74,18 +97,21 @@ export default class Setup extends Component {
           <Toggle options={
             [{
               text: 'HTML',
-              value: 'html'
+              value: 0
             },
               {
                 text: 'Google Tag Manager',
-                value: 'tag manager'
+                value: 1
               },
               {
                 text: 'Word Press',
-                value: 'word press'
+                value: 2
               }]}
-                  selectedValue="html"
-                  style = {{width: "345px"}}
+                  selectedValue={this.state.tab}
+                  style={{width: '345px', justifyContent: 'left'}}
+                  onClick={(value) => {
+                    this.setState({tab: value});
+                  }}
           >
           </Toggle>
           <div className={this.classes.secondStepText}>
@@ -98,6 +124,6 @@ export default class Setup extends Component {
           </Button>
         </div>)
       }
-    </div>;
+    </Page>;
   }
 }
