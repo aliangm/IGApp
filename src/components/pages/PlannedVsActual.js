12d790cecca728b64ee0841fcbbfe2838cd68789
Or Cherfas <or@infinigrow.com>
2018-09-17 15:13:09 +0300

    changed planned VS Actual page

--- src/components/pages/PlannedVsActual.js
+++ src/components/pages/PlannedVsActual.js
@@ -8,16 +8,20 @@ import SaveButton from 'components/pages/profile/SaveButton';
 import Button from 'components/controls/Button';
 import Textfield from 'components/controls/Textfield';
 import style from 'styles/plan/planned-actual-tab.css';
+import budgetsStyle from 'styles/plan/budget-table.css';
 import planStyles from 'styles/plan/plan.css';
-import { parsePlannedVsActual } from 'data/parsePlannedVsActual';
+import {parsePlannedVsActual} from 'data/parsePlannedVsActual';
 import Paging from 'components/Paging';
-import { getTitle } from 'components/utils/channels';
-import { formatChannels } from 'components/utils/channels';
+import {getTitle, isUnknownChannel} from 'components/utils/channels';
+import {formatChannels} from 'components/utils/channels';
+import {formatBudget} from 'components/utils/budget';
+import sumBy from 'lodash/sumBy';
+import icons from 'styles/icons/plan.css';
 
 export default class PlannedVsActual extends Component {
 
   style = style;
-  styles = [planStyles];
+  styles = [planStyles, budgetsStyle, icons];
 
   static defaultProps = {
     planUnknownChannels: [],
@@ -25,7 +29,7 @@ export default class PlannedVsActual extends Component {
     knownChannels: {},
     unknownChannels: {},
     hoverRow: void 0,
-    month: 0,
+    month: 0
   };
 
   constructor(props) {
@@ -49,12 +53,12 @@ export default class PlannedVsActual extends Component {
       knownChannels: data.actualChannelBudgets && data.actualChannelBudgets.knownChannels || {},
       unknownChannels: data.actualChannelBudgets && data.actualChannelBudgets.unknownChannels || {},
       approvedBudgets: data.approvedBudgets || [],
-      planUnknownChannels: data.unknownChannels || [],
+      planUnknownChannels: data.unknownChannels || []
     });
   }
 
   addChannel(event) {
-    if (!event){
+    if (!event) {
       var update = this.state.unknownChannels;
       update[this.state.otherChannel] = 0;
       this.setState({
@@ -70,7 +74,8 @@ export default class PlannedVsActual extends Component {
         }
         else {
           var alreadyExist = Object.keys(this.state.approvedBudgets[0]);
-          alreadyExist = alreadyExist.concat(Object.keys(this.state.knownChannels), Object.keys(this.state.unknownChannels));
+          alreadyExist =
+            alreadyExist.concat(Object.keys(this.state.knownChannels), Object.keys(this.state.unknownChannels));
           if (alreadyExist.indexOf(event.value) === -1) {
             var update = this.state.knownChannels;
             update[event.value] = 0;
@@ -85,9 +90,9 @@ export default class PlannedVsActual extends Component {
     this.setState({otherChannel: e.target.value});
   }
 
-  updateActual(key, value){
+  updateActual(key, value) {
     const title = getTitle(key);
-    if (!title){
+    if (!title) {
       let update = this.state.unknownChannels;
       update[key] = value;
       this.setState({unknownChannels: update});
@@ -102,19 +107,68 @@ export default class PlannedVsActual extends Component {
   getDates = () => {
     var dates = [];
     var monthNames = [
-      "Jan", "Feb", "Mar",
-      "Apr", "May", "Jun", "Jul",
-      "Aug", "Sep", "Oct",
-      "Nov", "Dec"
+      'Jan', 'Feb', 'Mar',
+      'Apr', 'May', 'Jun', 'Jul',
+      'Aug', 'Sep', 'Oct',
+      'Nov', 'Dec'
     ];
-    var planDate = this.state.planDate ? this.state.planDate .split("/") : null;
+    var planDate = this.state.planDate ? this.state.planDate.split('/') : null;
     if (planDate) {
       var date = new Date(planDate[1], planDate[0] - 1);
       dates.push(monthNames[date.getMonth()] + '/' + date.getFullYear().toString().substr(2, 2));
       return dates;
     }
-    else return [];
-  }
+    else {
+      return [];
+    }
+  };
+
+  getRowForData = (data, key, month, isTotalRow) => {
+    const actualText = formatBudget(data.actual);
+
+    return this.getTableRow(null, [
+      <div className={this.classes.cellItem}>
+        {!isTotalRow
+          ? <div className={budgetsStyle.locals.rowIcon}
+                 data-icon={!isUnknownChannel(data.key) ? `plan:${data.key}` : 'plan:other'}/>
+          : null
+        }
+        <div className={this.classes.channelName}>{data.channel}</div>
+        {data.isAutomatic ? <div className={this.classes.automaticLabel}>Automatic</div> : null}
+      </div>,
+      '$' + data.planned.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','),
+      <div className={this.classes.cellItem}>
+        {isTotalRow
+          ? <div className={this.classes.actualText} style={{
+            minWidth: '72px',
+            width: '50%'
+          }}>
+              {actualText}
+            </div>
+          : <Textfield style={{
+            minWidth: '72px',
+            width: '50%'
+          }} value={actualText} onChange={(e) => {
+            let value = parseInt(e.target.value.replace(/^\$/, '').replace(',', '')) || '';
+            this.updateActual(data.key, value);
+          }} disabled={data.isAutomatic}/>
+        }
+
+        {this.props.planDate === this.state.planDate
+          ? <div className={isTotalRow ? null : this.classes.expected}>
+            Expected: {formatBudget(Math.round(data.actual / this.props.calculatedData.extarpolateRatio))}
+          </div>
+          : null
+        }
+      </div>,
+      <div className={this.classes.cellItem}>
+        <div className={this.classes.channelName}>{formatBudget(data.planned - data.actual, true)}</div>
+      </div>
+    ], {
+      key: month + key,
+      className: isTotalRow ? this.classes.totalRow : null
+    });
+  };
 
   render() {
     let month;
@@ -123,42 +177,38 @@ export default class PlannedVsActual extends Component {
     let channelOptions = [];
     this.keys = this.getDates();
     month = this.keys[this.state.month];
-    const data = parsePlannedVsActual(this.state.approvedBudgets[0] || {}, this.state.planUnknownChannels[0] || {}, this.state.knownChannels, this.state.unknownChannels,
+    const data = parsePlannedVsActual(this.state.approvedBudgets[0] || {},
+      this.state.planUnknownChannels[0] || {},
+      this.state.knownChannels,
+      this.state.unknownChannels,
       {
         facebookadsapi: this.state.facebookadsapi,
         adwordsapi: this.state.adwordsapi,
         linkedinapi: this.state.linkedinapi,
         twitterapi: this.state.twitterapi
       }
-  );
+    );
     if (data) {
-      rows = data.map((item, i) => {
-        return this.getTableRow(null, [
-          <div className={ this.classes.cellItem }>
-            <div className={this.classes.channelName}>{item.channel}</div>
-            { item.isAutomatic ? <div className={ this.classes.automaticLabel }>Automatic</div> : null }
-          </div>,
-          '$' + item.planned.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','),
-          <div className={ this.classes.cellItem }>
-            <Textfield style={{
-              minWidth: '72px',
-              width: '100%'
-            }} value={ '$' + item.actual.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') } onChange={(e) => {
-              let value = parseInt(e.target.value.replace(/^\$/, '').replace(',','')) || '';
-              this.updateActual(item.key, value);
-            }}
-            disabled = { item.isAutomatic }/>
-          </div>
-        ], {
-          key: month + i
-        });
-      });
+
+      const totalData = {
+        key: 'total',
+        channel: 'Total',
+        isAutomatic: false,
+        planned: sumBy(data, item => item.planned),
+        actual: sumBy(data, item => item.actual)
+      };
+
+      const topTotalRow = this.getRowForData(totalData, '-1', month, true);
+      const bottomTotalRow = this.getRowForData(totalData, '-2', month, true);
+
+      rows = [topTotalRow, ...data.map((item, i) => this.getRowForData(item, i, month, false)), bottomTotalRow];
     }
 
     headRow = this.getTableRow(null, [
       'Channels',
       'Planned',
-      'Actual'
+      'Actual',
+      'Difference'
     ], {
       className: this.classes.headRow
     });
@@ -170,7 +220,9 @@ export default class PlannedVsActual extends Component {
         value.options.map(preventDuplicates);
       }
       else {
-        value.disabled = Object.keys(this.state.knownChannels).includes(value.value) || Object.keys(this.state.approvedBudgets[0] || {}).includes(value.value);
+        value.disabled =
+          Object.keys(this.state.knownChannels).includes(value.value) ||
+          Object.keys(this.state.approvedBudgets[0] || {}).includes(value.value);
         return value;
       }
     };
@@ -179,35 +231,36 @@ export default class PlannedVsActual extends Component {
 
     return <div>
       <Page>
-        <Title title="Planned VS Actual" subTitle="It is very important to keep the data credibility. To optimize your marketing planning every step of the way, InfiniGrow needs to know exactly what your actual marketing investments were (even if they aren’t 1:1 as recommended)."/>
-        <div className={ planStyles.locals.title }>
-          <Paging month={ this.state.planDate } pagingUpdateState={ this.pagingUpdateState } region={ this.state.region }/>
+        <Title title="Planned VS Actual"
+               subTitle="It is very important to keep the data credibility. To optimize your marketing planning every step of the way, InfiniGrow needs to know exactly what your actual marketing investments were (even if they aren’t 1:1 as recommended)."/>
+        <div className={planStyles.locals.title}>
+          <Paging month={this.state.planDate} pagingUpdateState={this.pagingUpdateState} region={this.state.region}/>
         </div>
-        <div className={ planStyles.locals.innerBox }>
-          <div className={ this.classes.wrap } ref="wrap">
-            <div className={ this.classes.box }>
-              <table className={ this.classes.table }>
+        <div className={planStyles.locals.innerBox}>
+          <div className={this.classes.wrap} ref="wrap">
+            <div className={this.classes.box}>
+              <table className={this.classes.table}>
                 {/*<col style={{ width: '50%' }} />
                  <col style={{ width: '25%' }} />
                  <col style={{ width: '25%' }} />*/}
                 <thead>
-                { headRow }
+                {headRow}
                 </thead>
-                <tbody className={ this.classes.tableBody }>
-                { rows }
+                <tbody className={this.classes.tableBody}>
+                {rows}
                 </tbody>
               </table>
             </div>
-            <div className={ this.classes.bottom }>
-              <MultiRow numOfRows={1} maxNumOfRows={1} >
+            <div className={this.classes.bottom}>
+              <MultiRow numOfRows={1} maxNumOfRows={1}>
                 {({index, data, update, removeButton}) => {
                   return <div style={{
                     paddingBottom: '25px',
                     width: '460px'
-                  }} className={ this.classes.channelsRow }>
+                  }} className={this.classes.channelsRow}>
                     <Select
-                      className={ this.classes.channelsSelect }
-                      selected={ -1 }
+                      className={this.classes.channelsSelect}
+                      selected={-1}
                       select={{
                         menuTop: true,
                         name: 'channels',
@@ -218,19 +271,19 @@ export default class PlannedVsActual extends Component {
                         },
                         options: channelOptions
                       }}
-                      onChange={ this.addChannel.bind(this) }
-                      label={ `Add a channel` }
-                      labelQuestion={ [''] }
-                      description={ ['Are there any channels you invested in the last month that weren’t recommended by InfiniGrow? It is perfectly fine; it just needs to be validated so that InfiniGrow will optimize your planning effectively.\nPlease choose only a leaf channel (a channel that has no deeper hierarchy under it). If you can’t find the channel you’re looking for, please choose “other” at the bottom of the list, and write the channel name/description clearly.']}
+                      onChange={this.addChannel.bind(this)}
+                      label={`Add a channel`}
+                      labelQuestion={['']}
+                      description={['Are there any channels you invested in the last month that weren’t recommended by InfiniGrow? It is perfectly fine; it just needs to be validated so that InfiniGrow will optimize your planning effectively.\nPlease choose only a leaf channel (a channel that has no deeper hierarchy under it). If you can’t find the channel you’re looking for, please choose “other” at the bottom of the list, and write the channel name/description clearly.']}
                     />
-                  </div>
+                  </div>;
                 }}
               </MultiRow>
-              { this.state.showText ?
-                <div className={ this.classes.channelsRow }>
+              {this.state.showText ?
+                <div className={this.classes.channelsRow}>
                   <Textfield style={{
                     width: '292px'
-                  }}  onChange={(e) => {
+                  }} onChange={(e) => {
                     this.addOtherChannel(e);
                   }}/>
                   <Button type="primary" style={{
@@ -241,11 +294,16 @@ export default class PlannedVsActual extends Component {
                   }}> Enter
                   </Button>
                 </div>
-                : null }
-              <div className={ this.classes.footer } style={{ marginTop: '150px' }}>
+                : null}
+              <div className={this.classes.footer} style={{marginTop: '150px'}}>
                 <SaveButton onClick={() => {
                   this.setState({saveFail: false, saveSuccess: false, updating: true}, () => {
-                    this.state.updateUserMonthPlan({actualChannelBudgets: {knownChannels: this.state.knownChannels, unknownChannels: this.state.unknownChannels}}, this.state.region, this.state.planDate, true)
+                    this.state.updateUserMonthPlan({
+                      actualChannelBudgets: {
+                        knownChannels: this.state.knownChannels,
+                        unknownChannels: this.state.unknownChannels
+                      }
+                    }, this.state.region, this.state.planDate, true)
                       .then((data) => {
                         this.setState({updating: false});
                         this.pagingUpdateState(data);
@@ -255,36 +313,37 @@ export default class PlannedVsActual extends Component {
                       this.props.updateUserAccount({'steps.plannedVsActual': true});
                     }
                   });
-                }} success={ this.state.saveSuccess} fail={ this.state.saveFail }/>
+                }} success={this.state.saveSuccess} fail={this.state.saveFail}/>
               </div>
             </div>
           </div>
         </div>
       </Page>
-    </div>
+    </div>;
   }
 
   getTableRow(title, items, props) {
-    return <tr {... props}>
-      { title != null ?
-        <td className={ this.classes.titleCell }>{ this.getCellItem(title) }</td>
-        : null }
+    return <tr {...props}>
+      {title != null ?
+        <td className={this.classes.titleCell}>{this.getCellItem(title)}</td>
+        : null}
       {
         items.map((item, i) => {
-          return <td className={ this.classes.valueCell } key={ i }>{
+          return <td className={this.classes.valueCell} key={i}>{
             this.getCellItem(item)
-          }</td>
+          }</td>;
         })
       }
-    </tr>
+    </tr>;
   }
 
   getCellItem(item) {
     let elem;
 
-    if (typeof item !== 'object' ) {
-      elem = <div className={ this.classes.cellItem }>{ item }</div>
-    } else {
+    if (typeof item !== 'object') {
+      elem = <div className={this.classes.cellItem}>{item}</div>;
+    }
+    else {
       elem = item;
     }
 
