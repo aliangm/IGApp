04653e36c44ffce4f1452db6a8e9880148a452af
Dor <dor@infinigrow.com>
2018-09-25 20:27:34 +0300

    a few styling changes

--- src/components/pages/profile/BackButton.js
+++ src/components/pages/profile/BackButton.js
@@ -10,7 +10,7 @@ export default class BackButton extends Component {
   style = style;
 
   render() {
-    return <Button type="primary" style={{
+    return <Button type="secondary" style={{
       width: '120px',
       letterSpacing: 0.075
     }} onClick={ this.props.onClick }>
