541283d353a70627fafd3f9b4bb1ab740fd0805f
Or Cherfas <or@infinigrow.com>
2018-10-18 13:37:52 +0300

    facebook api using AuthorizationPopup

--- src/components/pages/campaigns/importPopups/FacebookCampaignsPopup.js
+++ src/components/pages/campaigns/importPopups/FacebookCampaignsPopup.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Component from 'components/Component';
-import Page from 'components/Page';
 import Select from 'components/controls/Select';
 import style from 'styles/onboarding/onboarding.css';
-import Button from 'components/controls/Button';
 import serverCommunication from 'data/serverCommunication';
 import salesForceStyle from 'styles/indicators/salesforce-automatic-popup.css';
 import Title from 'components/onboarding/Title';
 import CRMStyle from 'styles/indicators/crm-popup.css';
+import AuthorizationIntegrationPopup from 'components/common/AuthorizationIntegrationPopup';
 
 export default class FacebookCampaignsPopup extends Component {
 
@@ -17,102 +16,36 @@ export default class FacebookCampaignsPopup extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      accounts: [],
-      code: null,
-      hidden: true
+      accounts: []
     };
   }
 
-  componentDidMount() {
-    if (!this.props.data) {
-      serverCommunication.serverRequest('get', 'facebookadsapi')
+  getUserData = () => {
+    return new Promise((resolve, reject) => {
+      serverCommunication.serverRequest('put',
+        'facebookadsapi',
+        JSON.stringify({accountId: this.state.selectedAccount}),
+        localStorage.getItem('region'))
         .then((response) => {
           if (response.ok) {
             response.json()
               .then((data) => {
-                this.setState({url: data});
+                this.props.setDataAsState(data);
+                resolve();
               });
           }
           else if (response.status == 401) {
             history.push('/');
           }
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-    }
-  }
-
-  getAuthorization = () => {
-    if (!this.props.data) {
-      const win = window.open(this.state.url);
-      const timer = setInterval(() => {
-        if (win.closed) {
-          clearInterval(timer);
-          const code = localStorage.getItem('code');
-          if (code) {
-            localStorage.removeItem('code');
-            this.setState({code: code});
-            this.getMapping(code);
+          else {
+            reject('Error getting data from facebook ads');
           }
-        }
-      }, 1000);
-    }
-    else {
-      this.getMapping();
-    }
+        });
+    });
   };
-
-  getMapping(code) {
-    serverCommunication.serverRequest('post',
-      'facebookadsapi',
-      JSON.stringify({code: code}),
-      localStorage.getItem('region'))
-      .then((response) => {
-        if (response.ok) {
-          response.json()
-            .then((data) => {
-              this.setState({accounts: data, hidden: false});
-            });
-        }
-        else if (response.status == 401) {
-          history.push('/');
-        }
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-  }
-
-  getUserData() {
-    serverCommunication.serverRequest('put',
-      'facebookadsapi',
-      JSON.stringify({accountId: this.state.selectedAccount}),
-      localStorage.getItem('region'))
-      .then((response) => {
-        if (response.ok) {
-          response.json()
-            .then((data) => {
-              this.props.setDataAsState(data);
-              this.close();
-            });
-        }
-        else if (response.status == 401) {
-          history.push('/');
-        }
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-  };
-
-  close() {
-    this.setState({hidden: true});
-    this.props.close();
-  }
 
   open = () => {
-    this.getAuthorization();
+    this.refs.authPopup.open();
   };
 
   render() {
@@ -127,25 +60,22 @@ export default class FacebookCampaignsPopup extends Component {
         }
       }
     };
-    return <div hidden={this.state.hidden}>
-      <Page popup={true} width={'680px'} innerClassName={salesForceStyle.locals.inner}
-            contentClassName={salesForceStyle.locals.content}>
-        <Title title="Choose Facebook Account"/>
-        <div className={this.classes.row}>
-          <Select {...selects.accounts} selected={this.state.selectedAccount} onChange={(e) => {
-            this.setState({selectedAccount: e.value});
-          }}/>
-        </div>
-        <div className={this.classes.footer}>
-          <div className={this.classes.footerLeft}>
-            <Button type="secondary" style={{width: '100px'}} onClick={this.close.bind(this)}>Cancel</Button>
-          </div>
-          <div className={this.classes.footerRight}>
-            <Button type="primary" style={{width: '100px'}} onClick={this.getUserData.bind(this)}>Done</Button>
-          </div>
-        </div>
-      </Page>
-    </div>;
+
+    return <AuthorizationIntegrationPopup ref='authPopup'
+                                          api='facebookadsapi'
+                                          afterDataRetrieved={this.afterDataRetrieved}
+                                          makeServerRequest={this.getUserData}
+                                          width='680px'
+                                          innerClassName={salesForceStyle.locals.inner}
+                                          contentClassName={salesForceStyle.locals.content}
+    >
+      <Title title="Choose Facebook Account"/>
+      <div className={this.classes.row}>
+        <Select {...selects.accounts} selected={this.state.selectedAccount} onChange={(e) => {
+          this.setState({selectedAccount: e.value});
+        }}/>
+      </div>
+    </AuthorizationIntegrationPopup>;
   }
 
 }
