5cb060e64ece0b2d74d661de8f641cf94dce0152
Or Cherfas <or@infinigrow.com>
2018-10-28 15:30:00 +0200

    added tag manager success message on alert

--- src/components/pages/attributionSetup/TagManagerAutomaticPopup.js
+++ src/components/pages/attributionSetup/TagManagerAutomaticPopup.js
@@ -83,6 +83,7 @@ export default class TagManagerAutomaticPopup extends Component {
         localStorage.getItem('region'))
         .then((response) => {
           if (response.ok) {
+            window.alert('Tag created successfully in tag manager')
             resolve(false);
           }
           else if (response.status == 401) {
