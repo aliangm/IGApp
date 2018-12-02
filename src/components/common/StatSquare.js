8afff396d1e1aed2a3ce21a26a7b1eda53bb3e79
Or Cherfas <or@infinigrow.com>
2018-12-02 17:18:12 +0200

    extracted basic square component

---
+++ src/components/common/StatSquare.js
@@ -0,0 +1,56 @@
+import React, { PropTypes } from "react";
+import Component from "components/Component";
+import style from "styles/stat-square.css";
+import ReactTooltip from 'react-tooltip';
+
+export default class StatSquare extends Component {
+
+  static propTypes = {
+    title:PropTypes.string.isRequired,
+    stat:PropTypes.string,
+    contextStat:PropTypes.node,
+    tooltipText: PropTypes.string,
+    showEmptyStat : PropTypes.bool,
+    emptyStatMessage: PropTypes.string
+  };
+
+  static defaultProps = {
+    tooltipText: '',
+    statWithArrow: false,
+    showEmptyStat: false,
+    contextText: ''
+  };
+
+  constructor(props){
+    super(props);
+    this.uniqueID = Math.random().toString(36).substr(2, 9);
+  }
+
+  style = style;
+
+  render() {
+    return <div className={ this.classes.col}>
+      <ReactTooltip place='bottom' effect='solid' id={this.uniqueID}/>
+      <div className={ this.classes.item }>
+        <div className={ this.classes.text }>{this.props.title}</div>
+        { this.props.showEmptyStat ?
+          <div>
+            <div className={ this.classes.center }>
+              <div className={ this.classes.sadIcon }/>
+            </div>
+            <div className={ this.classes.noMetrics }>
+              {this.props.emptyStatMessage}
+            </div>
+          </div>
+          :
+          <div>
+            <div className={ this.classes.number }>{this.props.stat}</div>
+            <div className={this.classes.context} data-tip={this.props.tooltipText} data-for={this.uniqueID}>
+              {this.props.contextStat}
+            </div>
+          </div>
+        }
+      </div>
+    </div>
+  }
+}
--- src/components/pages/dashboard/DashboardStatWithContext.js
+++ src/components/pages/dashboard/DashboardStatWithContext.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from "react";
 import Component from "components/Component";
 import style from "styles/dashboard/dashboard-stat-with-context.css";
-import ReactTooltip from 'react-tooltip';
+import StatSquare from 'components/common/StatSquare';
 
 export default class DashboardStatWithContext extends Component {
 
@@ -24,42 +24,19 @@ export default class DashboardStatWithContext extends Component {
     contextText: ''
   };
 
-  constructor(props){
-    super(props);
-    this.uniqueID = Math.random().toString(36).substr(2, 9);
-  }
-
   style = style;
 
   render() {
-    return <div className={ this.classes.col}>
-      <ReactTooltip place='bottom' effect='solid' id={this.uniqueID}/>
-      <div className={ this.classes.item }>
-        <div className={ this.classes.text }>{this.props.title}</div>
-        { this.props.showEmptyStat ?
-          <div>
-            <div className={ this.classes.center }>
-              <div className={ this.classes.sadIcon }/>
-            </div>
-            <div className={ this.classes.noMetrics }>
-              {this.props.emptyStatMessage}
-            </div>
-          </div>
-          :
-          <div>
-            <div className={ this.classes.number }>{this.props.stat}</div>
-            <div className={this.classes.context} data-tip={this.props.tooltipText} data-for={this.uniqueID}>
-              {this.props.contextStat ? <div className={this.classes.contextStat + ' ' + this.classes.contextText} data-negative={ this.props.isPositive ? null : 'negative' }>
-                  {this.props.statWithArrow ? <div className={this.classes.arrow} data-arrow-type={ !this.props.isPositive ? 'decline' : null } /> : ''}
-                  {this.props.contextStat}
-                </div> : ''
-              }
+    const stat = <div style={{display: "inline-flex"}}>
+      {this.props.contextStat ? <div className={this.classes.contextStat + ' ' + this.classes.contextText} data-negative={ this.props.isPositive ? null : 'negative' }>
+        {this.props.statWithArrow ? <div className={this.classes.arrow} data-arrow-type={ !this.props.isPositive ? 'decline' : null } /> : ''}
+        {this.props.contextStat}
+      </div> : ''
+      }
 
-              { ' '+ this.props.contextText }
-            </div>
-          </div>
-        }
-      </div>
-    </div>
+      { ' '+ this.props.contextText }
+    </div>;
+
+    return <StatSquare {...this.props} contextStat={stat}/>
   }
 }
