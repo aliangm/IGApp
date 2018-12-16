d97fdf26cb20358db84d458b4708ca26a0b105aa
Dor <dor@infinigrow.com>
2018-12-16 17:59:38 +0200

    navigate with real data

--- src/components/pages/dashboard/Navigate/Navigate.js
+++ src/components/pages/dashboard/Navigate/Navigate.js
@@ -1,73 +1,124 @@
-import React from 'react'
-import Component from 'components/Component'
-import PathChart from 'components/pages/dashboard/Navigate/PathChart'
-import style from 'styles/dashboard/navigate.css'
-import dashboardStyle from 'styles/dashboard/dashboard.css'
-import { getChannelsWithProps } from 'components/utils/channels'
+import React from 'react';
+import Component from 'components/Component';
+import PathChart from 'components/pages/dashboard/Navigate/PathChart';
+import style from 'styles/dashboard/navigate.css';
+import dashboardStyle from 'styles/dashboard/dashboard.css';
+import {getChannelIcon} from 'components/utils/channels';
+import {set} from 'lodash';
+import {getCommitedBudgets} from 'components/utils/budget';
+import {newFunnelMapping} from 'components/utils/utils';
 
 const formatForecastedIndicators = (forecastedIndicators) => forecastedIndicators.map((month) =>
-	Object.keys(month).reduce((res, key) => ({
-		...res,
-		[key]: month[key].committed,
-	}), { })
-)
-
-const formatObjectives = (objectives) =>
-	objectives
-		.filter((obj) => Object.keys(obj).length > 0)
-		.map((obj) => {
-			const name = Object.keys(obj)[0]
-			const data = obj[name]
-
-			return {
-				name,
-				target: data.target.value,
-			}
-		})
+  Object.keys(month).reduce((res, key) => ({
+    ...res,
+    [key]: month[key].committed
+  }), {})
+);
 
 export default class Navigate extends Component {
-	style = style
-	styles = [dashboardStyle]
 
-	// mock channels
-	get channels() {
-		return Object.keys(getChannelsWithProps()).map(channel => ({
-			key: channel,
-			score: Math.random() + 1, // from 1 to 2
-			icon: 'plan:' + channel
-		}))
-	}
+  style = style;
+  styles = [dashboardStyle];
 
-	// channel tooltip just for example
-	renderChannelTooltip = (channel) => (
-		<div className={this.classes.channelTooltip}>
-			<div className={this.classes.channelTooltipHeader}>{channel}</div>
-		</div>
-	)
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentObjective: 0,
+      months: 2
+    };
+  }
 
-	render() {
-		return (
-			<div className={this.classes.container}>
-				<div className={dashboardStyle.locals.wrap}>🚧</div>
-				<PathChart
-					data={{
-						future: formatForecastedIndicators(this.props.forecastedIndicators),
-						past: this.props.historyData.indicators,
-					}}
-					channels={{
-						future: this.channels.slice(0, 5),
-						past: this.channels.slice(5, 10),
-						present: this.channels.slice(10, 15),
-					}}
-					tooltip={{
-						future: this.renderChannelTooltip,
-						past: this.renderChannelTooltip,
-						present: this.renderChannelTooltip,
-					}}
-					objectives={formatObjectives(this.props.objectives)}
-					maxMonths={this.props.historyData.indicators.length}
-				/>
-			</div>
-		)
-	}
+  // componentDidMount() {
+  //   const objective = this.formatObjectives()[0];
+  //   this.setState({objective, months: objective.months + 1, currentObjective: 0});
+  // }
+
+  formatObjectives = () => {
+    const {collapsedObjectives, funnelObjectives} = this.props.calculatedData.objectives;
+    return collapsedObjectives
+      .filter((obj) => funnelObjectives.includes(obj.indicator))
+      .map((obj) => {
+        return {
+          name: obj.indicator,
+          target: obj.target
+        };
+      });
+  };
+
+  // channel tooltip just for example
+  renderChannelTooltip = (channel) => (
+    <div className={this.classes.channelTooltip}>
+      <div className={this.classes.channelTooltipHeader}>{channel}</div>
+    </div>
+  );
+
+  handleMonthsChange = (months) => this.setState({months});
+
+  handleObjectiveChange = (shift) => () => {
+    const objectivesCount = this.formatObjectives().length;
+
+    this.setState(({currentObjective}) => ({
+      currentObjective: (currentObjective + shift + objectivesCount) % objectivesCount
+    }));
+  };
+
+  render() {
+    const {forecastedIndicators, attribution: {channelsImpact}, historyData: {channelsImpact: historyChannelsImpact, indicators}, planBudgets, calculatedDatagit : {historyData: {historyDataLength}}} = this.props;
+    const {currentObjective, months} = this.state;
+    const parseChannelsImpact = (channelsImpact) => {
+      const impact = {};
+      channelsImpact.forEach(month => {
+        month && Object.keys(month).forEach(channel => {
+          set(impact, [channel], (impact[channel] || 0) + month[channel]);
+        });
+      });
+      return Object.keys(impact)
+        .map(channel => {
+          return {
+            key: channel,
+            score: Math.random() + 1, // from 1 to 2
+            icon: getChannelIcon(channel)
+          };
+        });
+    };
+
+    const objectives = this.formatObjectives();
+
+    const objectiveKey = objectives[currentObjective].name;
+    const mapping = newFunnelMapping[objectiveKey];
+
+    const channelsPresent = parseChannelsImpact([channelsImpact[mapping]]);
+    const channelsPast = parseChannelsImpact(historyChannelsImpact.map(month => month[mapping]).slice(0, months));
+
+    const committedBudgets = getCommitedBudgets(planBudgets);
+    const channelFuture = parseChannelsImpact(committedBudgets.slice(0, months));
+
+    return (
+      <div className={this.classes.container}>
+        <div className={dashboardStyle.locals.wrap}>🚧</div>
+        <PathChart
+          data={{
+            future: formatForecastedIndicators(forecastedIndicators),
+            past: indicators
+          }}
+          channels={{
+            future: channelFuture.slice(0, 5),
+            past: channelsPast.slice(0, 5),
+            present: channelsPresent.slice(0, 5)
+          }}
+          tooltip={{
+            future: this.renderChannelTooltip,
+            past: this.renderChannelTooltip,
+            present: this.renderChannelTooltip
+          }}
+          handleMonthsChange={this.handleMonthsChange}
+          handleObjectiveChange={this.handleObjectiveChange}
+          objectives={objectives}
+          currentObjective={currentObjective}
+          months={months}
+          maxMonths={historyDataLength}
+        />
+      </div>
+    );
+  }
 }
--- src/components/pages/dashboard/Navigate/PathChart.js
+++ src/components/pages/dashboard/Navigate/PathChart.js
@@ -1,265 +1,254 @@
-import React, { PropTypes } from 'react'
-import { ResponsiveContainer, LineChart, CartesianGrid, XAxis, YAxis, Line } from 'recharts'
-import ReactTooltip from 'react-tooltip'
-import Component from 'components/Component'
-import MonthsPopup from 'components/pages/dashboard/MonthsPopup'
-import style from 'styles/dashboard/navigate.css'
+import React, {PropTypes} from 'react';
+import {ResponsiveContainer, LineChart, CartesianGrid, XAxis, YAxis, Line} from 'recharts';
+import ReactTooltip from 'react-tooltip';
+import Component from 'components/Component';
+import MonthsPopup from 'components/pages/dashboard/MonthsPopup';
+import style from 'styles/dashboard/navigate.css';
 
-const styles = style.locals
-const LINE_COLOR = '#4d91fc'
+const styles = style.locals;
+const LINE_COLOR = '#4d91fc';
 
-const ObjectiveSelect = ({ objective, onNext, onPrev }) => {
-	return (
-		<div className={styles.objectiveSelect}>
-			<button className={styles.prevObjective} onClick={onPrev}/>
-			<div className={styles.objective}>
-				<div className={styles.objectiveName}>{objective.name}</div>
-				<div className={styles.objectiveTarget}>{objective.target}</div>
-			</div>
-			<button className={styles.nextObjective} onClick={onNext}/>
-		</div>
-	)
-}
+const ObjectiveSelect = ({objective, onNext, onPrev}) => {
+  return (
+    <div className={styles.objectiveSelect}>
+      <button className={styles.prevObjective} onClick={onPrev}/>
+      <div className={styles.objective}>
+        <div className={styles.objectiveName}>{objective.name}</div>
+        <div className={styles.objectiveTarget}>{objective.target}</div>
+      </div>
+      <button className={styles.nextObjective} onClick={onNext}/>
+    </div>
+  );
+};
 
-const Channels = ({ channels, tipSuffix }) => (
-	<div className={styles.channels}>
-		{channels.map((channel) => (
-			<div
-				key={channel.key}
-				className={styles.channel}
-				data-icon={channel.icon}
-				style={{ '--score': channel.score }}
-				data-tip={`${channel.key}::${tipSuffix}`}
-				data-place="right"
-			/>
-		))}
-	</div>
-)
+const Channels = ({channels, tipSuffix}) => (
+  <div className={styles.channels}>
+    {channels.map((channel) => (
+      <div
+        key={channel.key}
+        className={styles.channel}
+        data-icon={channel.icon}
+        style={{'--score': channel.score}}
+        data-tip={`${channel.key}::${tipSuffix}`}
+        data-place="right"
+      />
+    ))}
+  </div>
+);
 
 class PeriodsPanel extends Component {
-	render() {
-		const { months, onChange, maxMonths, channels } = this.props
+  render() {
+    const {months, onChange, maxMonths, channels} = this.props;
 
-		return (
-			<div className={styles.panelContainer}>
-				<div className={styles.past}>
-					<div className={styles.header}>
-						past {months} months
-						<div className={styles.editBlock}>
-							<button
-								className={styles.editButton}
-								onClick={() => this.pastPopup.open()}
-							/>
-							<MonthsPopup
-								className={styles.editPopup}
-								months={months}
-								maxMonths={maxMonths}
-								onChange={onChange}
-								getRef={ref => this.pastPopup = ref}
-							/>
-						</div>
-					</div>
-					<Channels channels={channels.past} tipSuffix="past"/>
-				</div>
-				<div className={styles.now}>
-					<div className={styles.nowHeader}>
-						now
-					</div>
-					<div className={styles.centerBlock}>
-						<div className={styles.centerUnderlay}/>
-						<div className={styles.centerDownCorner}/>
-						<div className={styles.centerUpCorner}/>
-					</div>
-					<div className={styles.centerLine}/>
-					<div className={styles.centerPointer}/>
-					<Channels channels={channels.present} tipSuffix="present"/>
-				</div>
-				<div className={styles.future}>
-					<div className={styles.header}>
-						next {months} months
-						<div className={styles.editBlock}>
-							<button
-								className={styles.editButton}
-								onClick={() => this.futurePopup.open()}
-							/>
-							<MonthsPopup
-								className={styles.editPopup}
-								months={months}
-								maxMonths={maxMonths}
-								onChange={onChange}
-								getRef={ref => this.futurePopup = ref}
-							/>
-						</div>
-					</div>
-					<Channels channels={channels.future} tipSuffix="future"/>
-				</div>
-			</div>
-		)
-	}
+    return (
+      <div className={styles.panelContainer}>
+        <div className={styles.past}>
+          <div className={styles.header}>
+            past {months} months
+            <div className={styles.editBlock}>
+              <button
+                className={styles.editButton}
+                onClick={() => this.pastPopup.open()}
+              />
+              <MonthsPopup
+                className={styles.editPopup}
+                months={months}
+                maxMonths={maxMonths}
+                onChange={onChange}
+                getRef={ref => this.pastPopup = ref}
+              />
+            </div>
+          </div>
+          <Channels channels={channels.past} tipSuffix="past"/>
+        </div>
+        <div className={styles.now}>
+          <div className={styles.nowHeader}>
+            now
+          </div>
+          <div className={styles.centerBlock}>
+            <div className={styles.centerUnderlay}/>
+            <div className={styles.centerDownCorner}/>
+            <div className={styles.centerUpCorner}/>
+          </div>
+          <div className={styles.centerLine}/>
+          <div className={styles.centerPointer}/>
+          <Channels channels={channels.present} tipSuffix="present"/>
+        </div>
+        <div className={styles.future}>
+          <div className={styles.header}>
+            next {months} months
+            <div className={styles.editBlock}>
+              <button
+                className={styles.editButton}
+                onClick={() => this.futurePopup.open()}
+              />
+              <MonthsPopup
+                className={styles.editPopup}
+                months={months}
+                maxMonths={maxMonths}
+                onChange={onChange}
+                getRef={ref => this.futurePopup = ref}
+              />
+            </div>
+          </div>
+          <Channels channels={channels.future} tipSuffix="future"/>
+        </div>
+      </div>
+    );
+  }
 }
 
-const TargetDot = ({ cx, cy, dotsCount, index }) => {
-	if (dotsCount !== index + 1) {
-		return null
-	}
+const TargetDot = ({cx, cy, dotsCount, index}) => {
+  if (dotsCount !== index + 1) {
+    return null;
+  }
 
-	return (
-		<svg
-			className={styles.pulse}
-			viewBox="0 0 6 6"
-			x={cx - 3}
-			y={cy - 3}
-			width={6}
-			height={6}
-		>
-			<circle className={styles.firstCircle} fill={LINE_COLOR} cx="3" cy="3" r="3"/>
-			<circle className={styles.secondCircle} fill={LINE_COLOR} cx="3" cy="3" r="3"/>
-			<circle className={styles.thirdCircle} fill={LINE_COLOR} cx="3" cy="3" r="3"/>
-			<circle fill={LINE_COLOR} cx="3" cy="3" r="3"/>
-		</svg>
-	)
-}
+  return (
+    <svg
+      className={styles.pulse}
+      viewBox="0 0 6 6"
+      x={cx - 3}
+      y={cy - 3}
+      width={6}
+      height={6}
+    >
+      <circle className={styles.firstCircle} fill={LINE_COLOR} cx="3" cy="3" r="3"/>
+      <circle className={styles.secondCircle} fill={LINE_COLOR} cx="3" cy="3" r="3"/>
+      <circle className={styles.thirdCircle} fill={LINE_COLOR} cx="3" cy="3" r="3"/>
+      <circle fill={LINE_COLOR} cx="3" cy="3" r="3"/>
+    </svg>
+  );
+};
 
 class PathChart extends Component {
-	style = style;
 
-	state = {
-		currentObjective: 0,
-		months: 2,
-	}
+  style = style;
 
-	get currentObjective() {
-		return this.props.objectives[this.state.currentObjective]
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	get chartData() {
-		const { months } = this.state
-		const { future, past } = this.props.data
+  get currentObjective() {
+    return this.props.objectives[this.props.currentObjective];
+  }
 
-		return [
-			...past.slice(-(months || 1)),
-			...future.slice(0, months),
-		].map((month, index) => ({
-			name: index,
-			value: month[this.currentObjective.name],
-		}))
-	}
+  get chartData() {
+    const {months, data: {future, past}} = this.props;
 
-	handleObjectiveChange = (shift) => () => {
-		const objectivesCount = this.props.objectives.length
+    return [
+      ...past.slice(-(months || 1)),
+      ...future.slice(0, months)
+    ].map((month, index) => ({
+      name: index,
+      value: month[this.currentObjective.name]
+    }));
+  }
 
-		this.setState(({ currentObjective }) => ({
-			currentObjective: (currentObjective + shift + objectivesCount) % objectivesCount,
-		}))
-	}
+  handlePrev = this.props.handleObjectiveChange(-1);
+  handleNext = this.props.handleObjectiveChange(1);
 
-	handlePrev = this.handleObjectiveChange(-1)
-	handleNext = this.handleObjectiveChange(1)
+  renderTooltip = (dataTip) => {
+    if (!dataTip) {
+      return null;
+    }
 
-	handleMonthsChange = (months) => this.setState({ months })
+    const [channel, suffix] = dataTip.split('::');
+    const tooltipRenderer = this.props.tooltip[suffix];
 
-	renderTooltip = (dataTip) => {
-		if (!dataTip) {
-			return null
-		}
+    return tooltipRenderer ? tooltipRenderer(channel) : null;
+  };
 
-		const [channel, suffix] = dataTip.split('::')
-		const tooltipRenderer = this.props.tooltip[suffix]
+  render() {
+    const {maxMonths, channels, handleMonthsChange, months} = this.props;
+    const {chartData, currentObjective} = this;
 
-		return tooltipRenderer ? tooltipRenderer(channel) : null
-	}
-
-	render() {
-		const { maxMonths, channels } = this.props
-		const { chartData, currentObjective } = this
-
-		return (
-			<div className={this.classes.chartWrapper}>
-				<div className={this.classes.chartContainer}>
-					<PeriodsPanel
-						maxMonths={maxMonths}
-						months={this.state.months}
-						onChange={this.handleMonthsChange}
-						channels={channels}
-					/>
-					<ObjectiveSelect
-						objective={currentObjective}
-						onNext={this.handleNext}
-						onPrev={this.handlePrev}
-					/>
-					<ResponsiveContainer width="100%" height="100%">
-						<LineChart
-							className={styles.chart}
-							key={`${currentObjective.name}-${chartData.length}`}
-							layout="vertical"
-							width={400}
-							height={700}
-							data={chartData}
-							margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-						>
-							<defs>
-								<linearGradient id="linear">
-									<stop offset="0%"   stopColor="rgba(77, 145, 252, 0.2)"/>
-									<stop offset="100%" stopColor={LINE_COLOR}/>
-								</linearGradient>
-							</defs>
-							<CartesianGrid stroke="rgba(255, 255, 255, 0.05)" horizontal={false}/>
-							<XAxis
-								type="number"
-								axisLine={false}
-								tickLine={false}
-								tick={{fontSize: '12px', fill: '#67759e', fontWeight: 600, letterSpacing: '0.1px'}}
-								tickMargin={10}
-							/>
-							<YAxis dataKey="name" type="category" reversed hide/>
-							<Line
-								type="monotone"
-								dataKey="value"
-								stroke="url(#linear)"
-								strokeWidth={2}
-								dot={<TargetDot dotsCount={chartData.length}/>}
-							/>
-						</LineChart>
-					</ResponsiveContainer>
-				</div>
-				<ReactTooltip
-					className={this.classes.tooltip}
-					getContent={this.renderTooltip}
-				/>
-			</div>
-		)
-	}
+    return (
+      <div className={this.classes.chartWrapper}>
+        <div className={this.classes.chartContainer}>
+          <PeriodsPanel
+            maxMonths={maxMonths}
+            months={months}
+            onChange={handleMonthsChange}
+            channels={channels}
+          />
+          <ObjectiveSelect
+            objective={currentObjective}
+            onNext={this.handleNext}
+            onPrev={this.handlePrev}
+          />
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              className={styles.chart}
+              key={`${currentObjective.name}-${chartData.length}`}
+              layout="vertical"
+              width={400}
+              height={700}
+              data={chartData}
+              margin={{top: 5, right: 30, left: 20, bottom: 5}}
+            >
+              <defs>
+                <linearGradient id="linear">
+                  <stop offset="0%" stopColor="rgba(77, 145, 252, 0.2)"/>
+                  <stop offset="100%" stopColor={LINE_COLOR}/>
+                </linearGradient>
+              </defs>
+              <CartesianGrid stroke="rgba(255, 255, 255, 0.05)" horizontal={false}/>
+              <XAxis
+                type="number"
+                axisLine={false}
+                tickLine={false}
+                tick={{fontSize: '12px', fill: '#67759e', fontWeight: 600, letterSpacing: '0.1px'}}
+                tickMargin={10}
+              />
+              <YAxis dataKey="name" type="category" reversed hide/>
+              <Line
+                type="monotone"
+                dataKey="value"
+                stroke="url(#linear)"
+                strokeWidth={2}
+                dot={<TargetDot dotsCount={chartData.length}/>}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <ReactTooltip
+          className={this.classes.tooltip}
+          getContent={this.renderTooltip}
+        />
+      </div>
+    );
+  }
 }
 
-const DataType = PropTypes.objectOf(PropTypes.number)
-const DataArrayType = PropTypes.arrayOf(DataType).isRequired
+const DataType = PropTypes.objectOf(PropTypes.number);
+const DataArrayType = PropTypes.arrayOf(DataType).isRequired;
 const ChannelType = PropTypes.shape({
-	key: PropTypes.string.isRequired,
-	score: PropTypes.number.isRequired,
-	icon: PropTypes.string.isRequired,
-})
-const ChannelsType = PropTypes.arrayOf(ChannelType).isRequired
+  key: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  icon: PropTypes.string.isRequired
+});
+const ChannelsType = PropTypes.arrayOf(ChannelType).isRequired;
 const ObjectiveType = PropTypes.shape({
-	name: PropTypes.string.isRequired,
-	target: PropTypes.number.isRequired,
-})
+  name: PropTypes.string.isRequired,
+  target: PropTypes.number.isRequired
+});
 
 PathChart.propTypes = {
-	data: PropTypes.shape({
-		past: DataArrayType,
-		future: DataArrayType,
-	}),
-	channels: PropTypes.shape({
-		past: ChannelsType,
-		future: ChannelsType,
-		present: ChannelsType,
-	}),
-	tooltip: PropTypes.shape({
-		past: PropTypes.func.isRequired,
-		future: PropTypes.func.isRequired,
-		present: PropTypes.func.isRequired,
-	}),
-	objectives: PropTypes.arrayOf(ObjectiveType).isRequired,
-}
+  data: PropTypes.shape({
+    past: DataArrayType,
+    future: DataArrayType
+  }),
+  channels: PropTypes.shape({
+    past: ChannelsType,
+    future: ChannelsType,
+    present: ChannelsType
+  }),
+  tooltip: PropTypes.shape({
+    past: PropTypes.func.isRequired,
+    future: PropTypes.func.isRequired,
+    present: PropTypes.func.isRequired
+  }),
+  objectives: PropTypes.arrayOf(ObjectiveType).isRequired
+};
 
-export default PathChart
+export default PathChart;
